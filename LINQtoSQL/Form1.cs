e2b1d0fdb06bc0c21550fa495f256577b2910ff8
Long Nguyen <zcmgyu@hotmail.com>
2016-04-08 19:39:48 +0700

    init Project

---
+++ LINQtoSQL/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQtoSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var db = new DatabaseDataContext();
+            var cate = new Category();
+            cate.CategoryName = textBox1.Text;
+            db.Categories.InsertOnSubmit(cate);
+            db.SubmitChanges();
+            MessageBox.Show("Saved");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var db = new DatabaseDataContext();
+            var query = from c in db.Categories
+                        select c;
+
+            comboBox1.DataSource = query.ToList();
+            comboBox1.DisplayMember = "CategoryName";
+        }
+    }
+}
